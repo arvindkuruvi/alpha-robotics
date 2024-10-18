5acd5f867fa0d8a6e6a0f5864d817ccf3007e80d
ARVIND\91861 <arvind.arirocker@gmail.com>
2024-10-18 22:09:38 +0530

    Missed file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./views/login/loginPage";
 import SignupPage from "./views/login/signupPage";
 import Landing from "./views/landing/Landing";
 import Pricing from "./views/pricing/Pricing";
 import ConnectBinance from "./views/binance/ConnectBinance";
-import Dashboard from "./views/testing-dashboard/Dashboard";
-import AuthRoutes from "./views/auth/AuthRoutes";
 import AuthProvider from "./views/auth/AuthProvider";
+import AuthRoutes from "./views/auth/AuthRoutes";
+import Dashboard from "./views/dashboard/Dashboard";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/connect-binance" element={<ConnectBinance />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
-    </BrowserRouter>
-    // <AuthProvider>
-    //   <AuthRoutes />
-    // </AuthProvider>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Landing />} />
+    //     <Route path="/dashboard" element={<Dashboard />} />
+    //     <Route path="/pricing" element={<Pricing />} />
+    //     <Route path="/connect-binance" element={<ConnectBinance />} />
+    //     <Route path="/login" element={<LoginPage />} />
+    //     <Route path="/signup" element={<SignupPage />} />
+    //   </Routes>
+    // </BrowserRouter>
+
+    <AuthProvider>
+      <AuthRoutes />
+    </AuthProvider>
   );
 }
 
